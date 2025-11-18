134dacde7bc101a5a4051c7d1693a2db5085f52c
Muhammad Adnan <vigodesign826@gmail.com>
2025-11-18 16:51:17 +0500

    astro template

--- astro.config.mjs
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://yourdomain.com',
-  compressHTML: true,
-  build: { inlineStylesheets: 'auto' },
-});
--- tailwind.config.cjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
