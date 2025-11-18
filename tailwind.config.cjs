04c2f2b3b108130165306a78d621a4428bc62bb7
Muhammad Adnan <vigodesign826@gmail.com>
2025-11-18 15:30:22 +0500

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,vue,svelte}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
